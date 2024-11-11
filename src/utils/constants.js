24be8e3b1926268332ef1088fb171dcde92e655f
khanhdev <106170850+khanhvuong63@users.noreply.github.com>
2024-11-11 21:22:01 +0700

    update env

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+let apiRoot = '';
+
+if (import.meta.env.VITE_BUILD_MODE === 'development') {
+  apiRoot = 'http://localhost:5000';
+} else if (import.meta.env.VITE_BUILD_MODE === 'production') {
+  apiRoot = 'https://api-chat-app-209j.onrender.com';
+}
+
+export const API_ROOT = apiRoot;
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import { API_ROOT } from './src/utils/constants'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy:{
-      "/api":{
-        target:"https://api-chat-app-209j.onrender.com",
-        changeOrigin:true,
-        secure: true,
-      }
-    }
-  }
-})
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   define: {
+//     'process.env': {},
+//   },
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     proxy:{
+//       "/api":{
+//         target: API_ROOT ,
+//         changeOrigin:true,
+//         secure: true,
+//       }
+//     }
+//   }
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Cấu hình Vite tùy theo môi trường
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? './' : '/', // Đặt base path cho production
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: mode === 'production' ? 'https://api-chat-app-209j.onrender.com' : 'http://localhost:5000',
+          changeOrigin: true,
+          secure: true,
+        },
+      },
+    },
+  };
+});
