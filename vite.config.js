2cbb7c1d30658a384d933162ec1cb4edf5f5485a
khanhdev <106170850+khanhvuong63@users.noreply.github.com>
2024-11-13 00:29:10 +0700

    fix config N

--- vite.config.js
+++ vite.config.js
@@ -20,24 +20,23 @@
 //   }
 // })
 
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    define: {
-        'process.env': process.env,
-      },
     plugins: [react()],
+    define: {
+      'process.env': process.env, // Cấu hình process.env để sử dụng các biến môi trường
+    },
     server: {
       port: 3000,
       proxy: {
         '/api': {
-          target: mode === 'development' ? 'http://localhost:5000' : 'https://api-chat-app-209j.onrender.com',
+          target: mode === 'production' ? 'https://api-chat-app-209j.onrender.com' : 'http://localhost:5000',
           changeOrigin: true,
-          secure: mode === 'production',
-          rewrite: (path) => path.replace(/^\/api/, ''), // Xóa `/api` khi gửi tới backend
+          secure: true,
         },
       },
     },
