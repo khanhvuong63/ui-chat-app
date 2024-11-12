98cc39ebe48e00af9a0342fbc888c876f24d5536
khanhdev <106170850+khanhvuong63@users.noreply.github.com>
2024-11-13 00:13:07 +0700

    fix config

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  define: {
-    'process.env': process.env,
-  },
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy:{
-      "/api":{
-        target: 'http://localhost:5000' ,
-        changeOrigin:true,
-        secure: true,
-      }
-    }
-  }
-})
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   define: {
+//     'process.env': process.env,
+//   },
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     proxy:{
+//       "/api":{
+//         target: 'http://localhost:5000' ,
+//         changeOrigin:true,
+//         secure: true,
+//       }
+//     }
+//   }
+// })
 
 
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// export default defineConfig(({ mode }) => {
-//   return {
-//     plugins: [react()],
-//     define: {
-//       'process.env': process.env,
-//     },
-//     server: {
-//       port: 3000,
-//       proxy: {
-//         '/api': {
-//           target: 'http://localhost:5000',
-//           changeOrigin: true,
-//           secure: mode === 'production',
-//           rewrite: (path) => path.replace(/^\/api/, ''),
-//         },
-//       },
-//     },
-//   };
-// });
+export default defineConfig(({ mode }) => {
+  return {
+    define: {
+        'process.env': process.env,
+      },
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: mode === 'development' ? 'http://localhost:5000' : 'https://api-chat-app-209j.onrender.com',
+          changeOrigin: true,
+          secure: mode === 'production',
+          rewrite: (path) => path.replace(/^\/api/, ''), // Xóa `/api` khi gửi tới backend
+        },
+      },
+    },
+  };
+});
